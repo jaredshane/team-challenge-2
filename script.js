69e1fd9e072bc775ac1506e7d5fa41788cc8d7f8
j-west <westja89@gmail.com>
2016-11-30 19:44:30 -0600

    Added semicolons to the end of several lines

--- script.js
+++ script.js
@@ -19,26 +19,26 @@ function btnEvent() {
 
 div1.addEventListener('click', function () {
 	addBorder(div1);
-	removeBorder(div2)
-	removeBorder(div3)
+	removeBorder(div2);
+	removeBorder(div3);
 	document.getElementById('form').style.visibility = "visible";
-})
+});
 
 div2.addEventListener('click', function () {
 	addBorder(div2);
-	removeBorder(div1)
-	removeBorder(div3)
+	removeBorder(div1);
+	removeBorder(div3);
 	document.getElementById('form').style.visibility = "visible";
-})
+});
 
 div3.addEventListener('click', function () {
 	addBorder(div3);
-	removeBorder(div2)
-	removeBorder(div1)
+	removeBorder(div2);
+	removeBorder(div1);
 	document.getElementById('form').style.visibility = "visible";
-})
+});
 
-btn.addEventListener('click', function () {
-	var userInput = document.getElementById('userInput').value
-	console.log(userInput)
-})
+btn.addEventListener('click', function() {
+  var userInput = document.getElementById('userInput').value;
+  console.log(userInput);
+});
